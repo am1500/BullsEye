e63d66329b38b3e6a4388b1632b26bf706370122
am1500 <carlilessard@wl-208-27.wpa.claremont.edu>
2014-09-03 22:21:04 -0700

    BullsEye Up to Page 89
    I continued learning a bit extra about iOS development

--- BullsEye/AMViewController.h
+++ BullsEye/AMViewController.h
@@ -8,6 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AMViewController : UIViewController
+@interface AMViewController : UIViewController <UIAlertViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UISlider *slider;
+@property (nonatomic, weak) IBOutlet UILabel *targetLabel;
+@property (nonatomic, weak) IBOutlet UILabel *scoreLabel;
+@property (nonatomic, weak) IBOutlet UILabel *roundLabel;
+
+- (IBAction)showAlert;
+- (IBAction)sliderMoved:(UISlider *)slider;
+- (IBAction)startOver;
 
 @end
--- BullsEye/AMViewController.m
+++ BullsEye/AMViewController.m
@@ -13,17 +13,106 @@
 @end
 
 @implementation AMViewController
+{
+    int _currentValue;
+    int _targetValue;
+    int _score;
+    int _round;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self startNewGame];
+    [self updateLabels];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showAlert;
+{
+    int difference = abs(_targetValue - _currentValue);
+    int points = 100 - difference;
+    _score += points;
+    
+    NSString *title;
+    if (difference == 0)
+    {
+        title = @"Perfect";
+        points += 100;
+    }
+    else if (difference < 5)
+    {
+        title = @"Almost there";
+        if (difference == 1) {
+            points += 50;
+        }
+    }
+    else if (difference < 10)
+    {
+        title = @"Pretty good";
+        
+    }
+    else {
+        title = @"Not even close";
+    }
+    
+    _score = points;
+    
+    NSString *message = [NSString stringWithFormat:
+                         @"You scored %d points",
+                         points];
+    
+    UIAlertView *alertView = [[UIAlertView alloc]
+                              initWithTitle:title
+                              message:message
+                              delegate:self
+                              cancelButtonTitle:@"Ok"
+                              otherButtonTitles:nil];
+    
+    [alertView show];
+    
+    [self startNewRound];
+    [self updateLabels];
+}
+
+- (IBAction)sliderMoved:(UISlider *)slider
+{
+    _currentValue = lroundf(slider.value);
+    
+}
+
+- (void)startNewRound
+{
+    _round += 1;
+    _targetValue = 1 + arc4random_uniform(100);
+    _currentValue = 50;
+    self.slider.value = _currentValue;
+    
+}
+
+- (void)updateLabels
+{
+    self.targetLabel.text = [NSString stringWithFormat:@"%d", _targetValue];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%d", _score];
+    self.roundLabel.text = [NSString stringWithFormat:@"%d", _round];
+}
+
+- (void)startOver
+{
+    [self startNewGame];
+    [self updateLabels];
+}
+
+- (void)startNewGame
+{
+    _round = 0;
+    _score = 0;
+    [self startNewRound];
 }
 
 @end
